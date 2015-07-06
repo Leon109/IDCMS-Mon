c0d73e8196b35407a46a98e33b4c4e6e3ac7ae9f
Leon109 <lsh8802@gmail.com>
2015-07-06 14:57:33 +0800

    web add

--- idcms/web/flask-web/app/main/froms.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField,PasswordField
-from wtforms.validators import Required
-
-
-class NameForm(Form):
-    name = StringField(validators=[Required()])
-    passwd = PasswordField(validators=[Required()])
-    submit = SubmitField('Read me')
