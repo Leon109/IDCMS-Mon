d2af89838409103a38846f7dea45d82cd5f9a389
Leon109 <lsh8802@gmail.com>
2015-07-08 10:13:42 +0800

    chang web

--- idcms/idcms_note/Web/flask-web/app/auth/views.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-from flask import render_template
-from . import auth
-from .forms import LoginForm
-
-@auth.route('/login')
-def login():
-    return render_template('loading.html')
--- idcms/idcms_note/Web/flask-web/manage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from app import create_app
-
-app = create_app('default')
-
-print app.url_map
-app.run(host='0.0.0.0', port=8000)
