9033828f942953850ce42134c93c335412ba7cfa
Leon109 <lsh8802@gmail.com>
2015-07-11 22:58:31 +0800

    "add cmdb"

--- idcms_note/Web/flask-web/app/cmdb/views.py
+++ idcms_note/Web/flask-web/app/cmdb/views.py
@@ -1,10 +1,9 @@
 #coding=utf-8
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 from . import cmdb
 #from .forms import LoginForm
-#@from ..models import User
-
+#from ..models import User
 
 @cmdb.route('/', methods=['GET'])
 @login_required
@@ -14,6 +13,7 @@ def index():
 @cmdb.route('/cmdb', methods=['GET', 'POST'])
 @login_required
 def cmdb():
+    uname =  current_user.username
     #form = LoginForm()
     #if form.validate_on_submit():
     #    user = User.query.filter_by(username=form.username.data).first()
@@ -22,5 +22,4 @@ def cmdb():
     #        return redirect(request.args.get('next') or url_for('main.index'))
     #    flash('Invalid username or password.')
     #return render_template('auth/loading.html', form=form)
-    return "cmdb"
-
+    return render_template('cmdb/cmdb.html', uname=uname)
