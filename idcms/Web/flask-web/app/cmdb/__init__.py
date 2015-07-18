96012f5f3a4306b0f746435739dc3acd3ad04c7c
Leon109 <lsh8802@gmail.com>
2015-07-18 10:56:28 +0800

    del idcms

--- idcms/Web/flask-web/app/cmdb/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-cmdb = Blueprint('cmdb', __name__)
-
-from . import views
--- idcms/Web/flask-web/manage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from app import create_app
-
-app = create_app('default')
-app.run(host='0.0.0.0', port=8001)
